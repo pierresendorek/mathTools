d8ef1c3d2dc4ce387e0b4bcf3af2e23f5a2b70c1
Pierre Sendorek <pierresendorek@gmail.com>
2016-02-07 19:32:34 +0100

    Update dynamicTimeWarping.jl

--- dynamicTimeWarping.jl
+++ dynamicTimeWarping.jl
@@ -1,9 +1,9 @@
 function dynamicTimeWarping(s::Array{Float64,2},t::Array{Float64,2})
-    
+
     function distance(x,y)
         return norm(x-y)
     end
-    
+
     n=size(s)[2]
     m=size(t)[2]
     dtw=Array(Float64,n,m)
@@ -13,24 +13,24 @@ function dynamicTimeWarping(s::Array{Float64,2},t::Array{Float64,2})
     for i in 1:m
         dtw[1,i]=Inf
     end
-    
+
     dtw[1,1]=0
-    
+
     for i in 2:n
-        for j in 2:m            
-           dtw[i,j]= distance(s[:,i],t[:,j]) + minimum([dtw[i-1,j],dtw[i,j-1],dtw[i-1,j-1]])    
+        for j in 2:m
+           dtw[i,j]= distance(s[:,i],t[:,j]) + minimum([dtw[i-1,j],dtw[i,j-1],dtw[i-1,j-1]])
         end
     end
-    
+
     tx=Array(Int64,0)
     ty=Array(Int64,0)
-    
+
     optI=n
     optJ=m
     unshift!(tx,optI)
-    unshift!(tY,optJ)
+    unshift!(ty,optJ)
     while(optI>1 && optJ>1)
-        i=argmin(dtw[optI-1,optJ],dtw[optI,optJ-1],dtw[optI-1,optJ-1])
+        i=indmin([dtw[optI-1,optJ],dtw[optI,optJ-1],dtw[optI-1,optJ-1]])
         if i==1
             optI-=1
         elseif i==2
@@ -40,8 +40,8 @@ function dynamicTimeWarping(s::Array{Float64,2},t::Array{Float64,2})
             optI-=1
         end
         unshift!(tx,optI)
-        unshift!(tY,optJ)      
-    end    
+        unshift!(ty,optJ)
+    end
     return (dtw[n,m],tx,ty)
 end
 
