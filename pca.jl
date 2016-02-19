eb3e329c91f19a64e8361c797609e0a0822e7c5c
Pierre Sendorek <pierresendorek@gmail.com>
2016-02-19 18:25:42 +0100

    Update pca.jl

--- pca.jl
+++ pca.jl
@@ -1,36 +1,28 @@
 using Distributions
 
-function computeCovariance()
-    # provisoire
-    M=randn(10,4)+100*ones(10,4)
-    return M'*M
-end
-
-
-function dimensionReductionMatrix(C)
-    maxLoss=0.1 # percentage of the sumsq
+function dimensionReductionMatrix(C,maxLoss=0.01)
+     # maxLoss=0.01 # percentage of the average sumsq
     d,R=eig(C)
     D=diagm(d)
     # C = R*D*R'
     cs=flipdim(cumsum(flipdim(d,1)),1)
-    i=2
-    while cs[i]>(1-maxLoss)*cs[1] && i<length(cs)# cs[1] = sum
-        i+=1
+    i=length(cs)
+    while cs[i]<(1-maxLoss)*cs[1] && i>1 # cs[1] = sum
+        i-=1
     end
-    i-=1
-
     T = (R')[i:end,:]
     return T
 end
 
 
 function testDimensionReduction()
-C=computeCovariance()
-T=dimensionReductionMatrix(C)
-x=rand(MvNormal(zeros(4),C))
+
+    M=randn(10,4)+5*ones(10,4)
+    C=M'*M
+    T=dimensionReductionMatrix(C)
+    x=rand(MvNormal(zeros(4),C))
     hatX=T\(T*x) # reconstruction from projection
     println(x)
     println(hatX)
 end
-
 
